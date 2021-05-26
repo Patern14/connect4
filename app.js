4fc42431693884e97b3205050240662329ef8117
Patern14 <paternmalle@gmail.com>
2021-05-26 15:44:02 +0100

    Board and selectors.

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 console.log("App.js CONNECTED")
 
+/* Selectors */
+
+const tableRow = document.getElementsByTagName("tr");
+const tableCell = document.getElementsByTagName("td");
+const tableSlot = document.querySelector(".slot");
+const playerTurn = document.querySelector(".player_turn");
+const resetGame = document.querySelector(".reset_game");
+
+for(let i = 0; 1 < tableCell.length; i++) {
+    tableCell[i].addEventListener("click", (e) => {
+        console.log("row: " + e.target.parentElement.rowIndex +", column: "+ e.target.cellIndex)
+    })
+}
+
+
+
+
 

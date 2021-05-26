cc447bcc1267b831093fa55a3ecdce9975812261
Patern14 <paternmalle@gmail.com>
2021-05-27 00:28:04 +0100

    Victory conditions.

--- app.js
+++ app.js
@@ -2,18 +2,144 @@ console.log("App.js CONNECTED")
 
 /* Selectors */
 
-const tableRow = document.getElementsByTagName("tr");
-const tableCell = document.getElementsByTagName("td");
-const tableSlot = document.querySelector(".slot");
+var tableRow = document.getElementsByTagName("tr");
+console.log(tableRow)
+var tableCell = document.getElementsByTagName("td");
+console.log(tableCell)
+var tableSlot = document.querySelector(".slot");
+console.log(tableSlot)
 const playerTurn = document.querySelector(".player_turn");
+console.log(playerTurn)
 const resetGame = document.querySelector(".reset_game");
 
-for(let i = 0; 1 < tableCell.length; i++) {
+while (!player1) {
+    //var player1 = prompt("Player 1: enter your name. You will be RED.")
+    var player1 = "Player 1";
+}
+player1Color = "red";
+
+while (!player2) {
+    //var player2 = prompt("Player 2: enter your name. You will be YELLOW.")
+    var player2 = "Player 2"
+}
+player2Color = "yellow";
+
+var currentPlayer = 1;
+//playerTurn.textContent = `${player1}'s turn`;
+playerTurn.textContent = `${player1}'s turn.`;
+console.log(playerTurn)
+
+const checkColor = (one, two, three, four) => {
+    return (one === two && one === three && one === four && one !== "white");
+}
+
+const checkHorizontal = () => {
+    for (let row = 0; row < tableRow.length; row++) {
+        for (let col = 0; col < 4; col++) {
+            if (checkColor( tableRow[row].children[col].style.backgroundColor, 
+                            tableRow[row].children[col+1].style.backgroundColor, 
+                            tableRow[row].children[col+2].style.backgroundColor, 
+                            tableRow[row].children[col+3].style.backgroundColor)
+            ) { return true }
+        }
+    }
+}
+
+const checkVertical = () => {
+    for (let col = 0; col < 7; col++) {
+        for (let row = 0; row < 3; row++) {
+            if (checkColor( tableRow[row].children[col].style.backgroundColor, 
+                            tableRow[row+1].children[col].style.backgroundColor, 
+                            tableRow[row+2].children[col].style.backgroundColor, 
+                            tableRow[row+3].children[col].style.backgroundColor)
+            ) { return true }
+        } 
+    }
+}
+const checkDiagonalToRight = () => {
+    for (let col = 0; col < 4; col++) {
+        for (row = 0; row < 3; row++) {
+            if (checkColor( tableRow[row].children[col].style.backgroundColor, 
+                            tableRow[row+1].children[col+1].style.backgroundColor, 
+                            tableRow[row+2].children[col+2].style.backgroundColor, 
+                            tableRow[row+3].children[col+3].style.backgroundColor)
+            ) { return true }
+        }
+    }
+}
+
+const checkDiagonalToLeft = () => {
+    for (let col = 0; col < 4; col++) {
+        for (row = 5; row > 2; row--) {
+            if (checkColor( tableRow[row].children[col].style.backgroundColor, 
+                            tableRow[row-1].children[col+1].style.backgroundColor, 
+                            tableRow[row-2].children[col+2].style.backgroundColor, 
+                            tableRow[row-3].children[col+3].style.backgroundColor)
+            ) { return true }
+        }
+    }
+}
+
+const checkDraw = () => {
+    let fullSlot = [];
+    for (let i = 0; i < tableCell.length; i++) {
+        if (tableCell[i].style.backgroundColor !== "white") {
+            fullSlot.push(tableCell[i]);
+        }
+    }
+    if (fullSlot.length === tableCell.length) {
+        return true;
+    }
+}
+
+const changeColor = (e) => {
+    let column = e.target.cellIndex;
+    let row = [];
+    
+    for (let i = 5; i > -1; i--) {       // i=5 because we start at the table bottom.
+        if (tableRow[i].children[column].style.backgroundColor == "white") {
+            row.push(tableRow[i].children[column]);
+            if (currentPlayer === 1) {
+                row[0].style.backgroundColor = player1Color;
+                if (checkHorizontal() || checkVertical() || checkDiagonalToRight() || checkDiagonalToLeft()) {
+                    playerTurn.textContent = `${player1} WINS!`;
+                    playerTurn.style.color = player1Color;
+                    return (alert(`${player1} WINS!`));
+                } else if (checkDraw()) {
+                    playerTurn.textContent = "It's a DRAW...";
+                    return (alert("DRAW!"));
+                } else {
+                    playerTurn.textContent = `${player2}'s turn.`;
+                    return currentPlayer = 2;
+                }
+            } else {
+                row[0].style.backgroundColor = player2Color;
+                if (checkHorizontal() || checkVertical() || checkDiagonalToRight() || checkDiagonalToLeft()) {
+                    playerTurn.textContent = `${player2} WINS!`;
+                    playerTurn.style.color = player2Color;
+                    return (alert(`${player2} WINS!`));
+                } else if (checkDraw()) {
+                    playerTurn.textContent = "It's a DRAW...";
+                    return (alert("DRAW!"));
+                } else {
+                    playerTurn.textContent = `${player1}'s turn.`;
+                    return currentPlayer = 1;
+                }
+            }
+        }
+    }
+}
+
+Array.prototype.forEach.call(tableCell, (cell) => {
+    cell.addEventListener("click", changeColor);
+    cell.style.backgroundColor = "white";
+})
+
+for (let i = 0; 1 < tableCell.length; i++) {     // Just to know what cell we click
     tableCell[i].addEventListener("click", (e) => {
         console.log("row: " + e.target.parentElement.rowIndex +", column: "+ e.target.cellIndex)
     })
 }
-
 
 
 
